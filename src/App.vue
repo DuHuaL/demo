a0e327e4c2b52b77df31aa4c059a130509e6745c
DuHuaL <469580396@qq.com>
2019-11-19 14:26:50 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    购物车：<br>
+    <num :count="value"></num>
+  </div>
+</template>
+
+<script>
+import Num from '@/components/num';
+export default {
+  name: 'App',
+  components: {
+    Num
+  },
+  data() {
+    return {
+      value: 1
+    };
+  }
+};
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ src/components/num.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button>+</button>
+    <input type="text" v-model="count" style="width: 50px">
+    <button>-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  props: ['count']
+};
+</script>
+
+<style>
+
+</style>
